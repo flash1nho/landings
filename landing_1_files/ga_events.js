7d199742a77510625381eedc24613d26c46de261
Igor Okunev <okunev1988@gmail.com>
2019-08-19 11:02:41 +0500

    feature(services): add html

--- landing_1_files/ga_events.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(document).on('click', '.js-ga-event', function() {
-    $this = $(this);
-
-    _gaq.push(['_trackEvent', $this.data('category'), $this.data('action')]);
-  });
-});
